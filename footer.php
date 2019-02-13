98ea9ad8e247f6b5fafeb83baaf6424a672f8a69
Islam Shaaban Hussien <36308342+islamshaabaan@users.noreply.github.com>
2019-02-13 16:20:29 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<div class="jumbotron text-center" style="margin-bottom: 0; background-color: rgb(32,32,32);">
+	<footer class="footer">
+      <div class="container">
+        <span class="text-muted">Copyright 2018, Software Engineering Course, ASUENG.</span>
+      </div>
+    </footer>
+  <p style="color: white;"><span>&copy;</span> <a href="SE2018G01.ml" style="color: white;">SE2018G01.ml</a></p>
+</div>
+    </html>
