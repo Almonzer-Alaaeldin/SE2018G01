b2c13d0f584d6c5de13e76d0bc71a83051434e43
Islam Shaaban Hussien <36308342+islamshaabaan@users.noreply.github.com>
2019-02-13 16:19:31 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Developer Registration</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+   <link href = "/bootstrap/css/bootstrap.min.css" rel = "stylesheet">
+      <script src = "/scripts/jquery.min.js"></script>
+      <script src = "/bootstrap/js/bootstrap.min.js"></script>
+  <!------ Include the above in your HEAD tag ---------->
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+  <link rel="stylesheet" href="styles.css">
+  
+</head>
+
+
+    </html>
