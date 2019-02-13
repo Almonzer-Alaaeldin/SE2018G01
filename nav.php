dde00f5a7045b371cb927977580611670ad0b2d8
Islam Shaaban Hussien <36308342+islamshaabaan@users.noreply.github.com>
2019-02-13 16:19:47 +0200

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <a class="navbar-brand" href="#">System Advertisement Website</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sign in</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav> 
+    </html>
+    
